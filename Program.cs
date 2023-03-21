6f791c60f5c51ac6d497c31d55b141c252131368
nitesh-abjima <128471408+nitesh-abjima@users.noreply.github.com>
2023-03-21 18:50:30 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,6 +8,24 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int x = 5, y = 6, z = 50;
+            Console.WriteLine(x + y + z);
+
+            int a, b, c;
+            a = b = c = 50;
+            Console.WriteLine(x + y + z);
+
+            int myNum = 15;
+            myNum = 20; 
+            Console.WriteLine(myNum);
+
+            int x = 20;
+            int y = 18;
+            if (x > y)
+            {
+                Console.WriteLine("x is greater than y");
+            }
         }
     }
 }
